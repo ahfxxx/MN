7ab07518cb652c995aafb0c228dab00544067382
ahfxxx <akram.jannah@gmail.com>
2018-03-18 19:56:01 +0800

    Update bltg_install.sh

--- bltg_install.sh
+++ bltg_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
 mv uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
