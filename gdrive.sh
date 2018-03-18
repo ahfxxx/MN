0e37a4d172bcd808b2780f971157840366a1777b
akramjannah <akram.jannah@gmail.com>
2018-03-18 22:56:44 +0800

    Delete gdrive.sh

--- gdrive.sh
+++
@@ -1,6 +0,0 @@
-cd ~
-https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
-mv uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download gdrive
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
-gdrive list
