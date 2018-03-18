5bedb914d7a75a0cd241c5c36131c778c684956a
akramjannah <akram.jannah@gmail.com>
2018-03-18 23:06:57 +0800

    Update bltg_install.sh

--- bltg_install.sh
+++ bltg_install.sh
@@ -16,12 +16,12 @@ MNPORT="17127"
 THEDATE=`date +"%Y%m%d-%H%M"`
 BACKUPWALLET="wallet.dat-${COIN}-${IP_ADD}-${THEDATE}.txt"
 
-sudo touch /var/swap.img
-sudo chmod 600 /var/swap.img
-sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-mkswap /var/swap.img
-sudo swapon /var/swap.img
-sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+#sudo touch /var/swap.img
+#sudo chmod 600 /var/swap.img
+#sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+#mkswap /var/swap.img
+#sudo swapon /var/swap.img
+#sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
